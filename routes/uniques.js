36eee57ff2940eb1053dc0ec8fa488c628eb670a
Dale Kunce <dale@normalhabit.com>
2013-10-28 22:30:59 -0400

    adding error handling for null places

--- routes/uniques.js
+++ routes/uniques.js
@@ -7,12 +7,12 @@ settings = require('../settings');
 
 //Configure Loggly (logging API)
 var config = {
-	subdomain: settings.loggly.subdomain,
-	auth: {
-		username: settings.loggly.username,
-		password: settings.loggly.password
-	},
-	json: true
+    subdomain: settings.loggly.subdomain,
+    auth: {
+        username: settings.loggly.username,
+        password: settings.loggly.password
+    },
+    json: true
 };
 
 //Loggly client
@@ -23,78 +23,90 @@ ga.initialize(settings.ga.key, 'webviz.redcross.org', function () {
 });
 
 exports.gdpcClavin = function(req,res) {
-	var docs = req.body;
-	var queryResponse = [];
+    var docs = req.body;
+    var queryResponse = [];
 
-	if (docs.length > 1) {
-		res.end('Only submit one document at a time.');
-	}
+    if (docs.length > 1) {
+        res.end('Only submit one document at a time.');
+    }
 
-	var gdpcCommand = 'sudo cp '+ settings.filepath.filename + ' ' + settings.filepath.clavin + ' && cd ../CLAVIN && MAVEN_OPTS="-Xmx2048M" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.gdpc"';
-	var exec = require('child_process').exec;
-	
+    var gdpcCommand = 'sudo cp '+ settings.filepath.filename + ' ' + settings.filepath.clavin + ' && cd ../CLAVIN && MAVEN_OPTS="-Xmx2048M" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.gdpc"';
+    var exec = require('child_process').exec;
 
-	fs.writeFile(settings.filepath.filename, docs[0].text, function(err) {
-		if(err) {
-			log(err);
-		} else {
-			// log("The file " + docs[0].name + " was saved!");
-		}
-	});
+    fs.writeFile(settings.filepath.filename, docs[0].text, function(err) {
+        if(err) {
+            log(err);
+        } else {
+            log("The file " + docs[0].name + " was saved!");
+        }
+    });
 
-	var parseClavin = function(clavin, callback) {
-		async.waterfall([
-			function(callback) {
-				var makePlacesResult;
-				async.map(clavin, makePlaces, function(err, result) {
-					makePlacesResult = result;
-				});
+    //create the response
+    var uniques = {};
+    uniques.documentName = docs[0].name;
+    uniques.date = new Date();
+    uniques.resolvedLocations = [];
 
-				callback(null, makePlacesResult);
-			},
-			function(places, callback){
-		    	//create final response
-		    	var uniques = {};
-		    	uniques.documentName = docs[0].name;
-		    	uniques.date = new Date();
-		    	uniques.resolvedLocations = [];
+    var parseClavin = function(clavin, callback) {
+        log('starting waterfall');
+        async.waterfall([
+            function(callback) {
+                var makePlacesResult;
+                async.map(clavin, makePlaces, function(err, result) {
+                    makePlacesResult = result;
+                });
+                log('step 1: \n' + makePlacesResult);
+                callback(null, makePlacesResult);
+            },
+            function(places, callback){
 
-		    	//locations array
-		  		for(i=0;i<places.length;i++){
-		  			var location = {};
-		  			location.name = places[i];
-		  			location.centroid = {'lat': '', 'lng':''};
-		  			uniques.resolvedLocations.push(location);
-		  		}
+                //locations array
+                for(i=0;i<places.length;i++){
+                    var location = {};
+                    location.name = places[i];
+                    location.centroid = {'lat': '', 'lng':''};
+                    uniques.resolvedLocations.push(location);
+                }
+                log('step 2 \n' + JSON.stringify(uniques));
+                callback(null, uniques);
+            }
+        ], function (err, result) {
+            callback(result);
+        });
+    }
 
-		    	callback(null, uniques);
-		    }
-		], function (err, result) {
-		   callback(result);  
-		});
-	}
+    var myObj = {};
 
-	var myObj = {};
+    myObj.list = function(callback){
+        var result;
+        exec(gdpcCommand, function (error, stdout, stderr) {
+            if (error) {
+                log(error);
+            } else {
+                callback(stdout);
+            }
+        });
+    }
 
-	myObj.list = function(callback){
-		var result;
-		exec(gdpcCommand, function (error, stdout, stderr) {
-			callback(stdout);
-		});
-	}
+    myObj.list(function (stdout) {
+        log(stdout);
+        var cS = stdout.indexOf('Resolved');
+        var cE = stdout.indexOf('All Done')-3;
+        var clavinRep = stdout.substring(cS,cE).split('#$#$');
+        // clavinRep = clavinRep.split('#$#$');
+        if (cS == -1) {
+            uniques.resolvedLocations.push('None Found');
+            res.jsonp(uniques);
+        } else {
+            log('calling parse Clavin');
+            parseClavin(clavinRep, function(places) {
+                res.jsonp(places);
+            });
+        }
 
-	myObj.list(function (stdout) {
-		var cS = stdout.indexOf('Resolved');
-		var cE = stdout.indexOf('All Done')-3;
-		var clavinRep = stdout.substring(cS,cE).split('#$#$');
-		// clavinRep = clavinRep.split('#$#$');
-
-		//log(stdout);
-
-		parseClavin(clavinRep, function(places) {
-			res.jsonp(places);
-		});
-	});
+        //log(stdout);
+        
+    });
 
 }
 
@@ -106,30 +118,30 @@ function log(message) {
 }
 
 function makePlaces(places, callback) {
-		var placesArr = [];
+    var placesArr = [];
 
-		var pS1 = places.indexOf('{') + 1;
-		var pE1 = places.indexOf('(') - 1;
-		var pS2 = pE1 + 2;
-		var pE2 = places.indexOf(',');
-		var pS3 = pE2 + 1;
-		var pE3 = places.indexOf(')');
+    var pS1 = places.indexOf('{') + 1;
+    var pE1 = places.indexOf('(') - 1;
+        var pS2 = pE1 + 2;
+        var pE2 = places.indexOf(',');
+        var pS3 = pE2 + 1;
+        var pE3 = places.indexOf(')');
 
-		var place1 = places.substring(pS1,pE1);
-		var place2 = places.substring(pS2,pE2);
-		var place3 = places.substring(pS3,pE3);
+        var place1 = places.substring(pS1,pE1);
+        var place2 = places.substring(pS2,pE2);
+        var place3 = places.substring(pS3,pE3);
 
-		if (place2 != 'United States') {
-			if (place3 != ' 00') {
-				placesArr.push(place1, place2, place3);
-			} else {
-				placesArr.push(place1, place2);
-			}
-			
-		} else {
-			placesArr.push(place1 + ',' + place3, place2);
-		}
+        if (place2 != 'United States') {
+            if (place3 != ' 00') {
+                placesArr.push(place1, place2, place3);
+            } else {
+                placesArr.push(place1, place2);
+            }
 
-		// log(placesArr);
-		callback(null, placesArr);
-}
+        } else {
+            placesArr.push(place1 + ',' + place3, place2);
+        }
+
+                // log(placesArr);
+                callback(null, placesArr);
+            }
